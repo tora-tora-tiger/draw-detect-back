2dda300593de6b9c0982cb38645537992074f761
tora <bmvdsfy@gmail.com>
2025-12-23 22:08:44 +0900

    chore: vitest の設定を型検査をするように変更

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    typecheck: {
+      enabled: true,
+      include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+      ignoreSourceErrors: true,
+    },
+  },
 });
