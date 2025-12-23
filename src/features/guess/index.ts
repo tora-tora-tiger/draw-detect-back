f7a650cd88c6a84f3332409973bfab853ad3cfb0
tora <bmvdsfy@gmail.com>
2025-12-23 19:56:48 +0900

    ファイル構造を決定

---
+++ src/features/guess/index.ts
@@ -0,0 +1,6 @@
+import { factory } from "@/factory";
+
+// Hono インスタンスを作成
+const guess = factory.createApp();
+
+export default guess;
---
+++ src/features/topics/index.ts
@@ -0,0 +1,6 @@
+import { factory } from "@/factory";
+
+// Hono インスタンスを作成
+const topics = factory.createApp();
+
+export default topics;
