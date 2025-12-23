a8dd484d663254eaa0f53a7aebfd1d0dd5feae5b
tora <bmvdsfy@gmail.com>
2025-12-23 23:04:16 +0900

    chore: test 時に環境変数を使えるようにする

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    environment: "node", // テスト環境では Node.js 環境を使用（process.env にアクセス可能）
+    env: dotenv.config({ path: ".env" }).parsed,
     typecheck: {
       enabled: true,
       include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)"],
