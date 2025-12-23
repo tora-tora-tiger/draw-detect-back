e702b44cecff823b0d339edaa7501e2791588340
tora <bmvdsfy@gmail.com>
2025-12-24 01:42:32 +0900

    feat(guess): お題を推測するAPIを作成

--- src/features/guess/index.ts
+++ src/features/guess/index.ts
@@ -1,6 +1,32 @@
 import { factory } from "@/factory";
+import { createLLMClient } from "@/lib/llm/client";
+import { createLLM } from "@/lib/llm/llm.factory";
+import { ok } from "@/utils/response";
 
 // Hono インスタンスを作成
 const guess = factory.createApp();
+
+guess.post("/", async c => {
+  const llm = createLLM(c);
+  const llmClient = createLLMClient(llm);
+
+  try {
+    const body = await c.req.parseBody();
+    const topic = body.topic as string;
+    const image = body.image as File;
+
+    const guessWord = await llmClient.guessWord(topic, image);
+
+    return c.json(ok({ guessWord }));
+  } catch {
+    // console.error("Error in guess endpoint:", error);
+    return c.json(
+      {
+        type: "INTERNAL_SERVER_ERROR",
+      },
+      500,
+    );
+  }
+});
 
 export default guess;
