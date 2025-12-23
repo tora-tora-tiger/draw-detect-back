09b2bae6ab81eebadaa71b7f0d7a337372ccdc90
tora <bmvdsfy@gmail.com>
2025-12-23 22:19:20 +0900

    feat(llm): `createLLMClient` の実装

--- src/lib/llm/type.ts
+++ src/lib/llm/type.ts
@@ -1,6 +1,21 @@
+interface Attachment {
+  filename: string;
+  mimeType: string;
+  data: File;
+}
+
+interface Prompt {
+  text: string;
+  attachments?: Attachment[];
+}
+
+interface LLM {
+  ask(prompt: Prompt): Promise<string>;
+}
+
 interface LLMClient {
   generateTopic(topic: string): Promise<string>;
   guessWord(topic: string, image: File): Promise<string>;
 }
 
-export type { LLMClient };
+export type { Prompt, Attachment, LLM, LLMClient };
